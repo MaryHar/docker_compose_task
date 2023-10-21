c1a2c7259affde1fa771b5c81245a56a7810bd4b
Mary <meri01harutyunyan.2002@gmail.com>
2023-10-21 21:34:38 +0400

    Change app.js

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-  const express = require('express');
-  const mysql = require('mysql');
-  const app = express();
-  
-  const db = mysql.createConnection({
-    host: 'db', // This should match the service name in the docker-compose file
-    user: 'root',
-    password: 'examplepassword',
-    database: 'myappdb',
-  });
-  
-  db.connect((err) => {
-    if (err) {
-      console.error('Database connection failed:', err);
-    } else {
-      console.log('Database connected');
-    }
-  });
-  
-  app.get('/', (req, res) => {
-    res.send('Hello, Docker Compose with Node.js and MySQL!');
-  });
-  
-  app.listen(3000, () => {
-    console.log('Node.js app is listening on port 3000');
+const express = require('express');
+const mysql = require('mysql2/promise');
+const app = express();
+
+async function waitForDatabase() {
+  try {
+    const db = await mysql.createConnection({
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+    });
+    console.log('Database connected');
+    return db;
+  } catch (err) {
+    console.error('Database connection failed. Retrying in 2 seconds...');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    return waitForDatabase();
+  }
+}
+
+waitForDatabase()
+  .then((db) => {
+    app.locals.db = db;
+
+    app.get('/', (req, res) => {
+      res.send('Hello, Docker Compose with Node.js and MySQL!');
+    });
+
+    app.listen(3000, () => {
+      console.log('Node.js app is listening on port 3000');
+    });
+  })
+  .catch((err) => {
+    console.error('Failed to connect to the database:', err);
   });
